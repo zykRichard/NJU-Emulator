9797b03aec490be77a89553dafe20d8eacce7121
tracer-ics2022 <tracer@njuics.org>
2022-09-06 20:23:36 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:23:36 up 50 min,  1 user,  load average: 0.45, 0.35, 0.30

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -75,6 +75,7 @@ static int cmd_si(char *args) {
 static int cmd_info(char *args) {
   char *arg = strtok(NULL, " ");
   if(strcmp(arg, "r") == 0) {
+    printf("get r");
     isa_reg_display();
     return 0;
   }
