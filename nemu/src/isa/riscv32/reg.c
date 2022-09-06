80bac63c15163e5807b322522083f34e4cb533a2
tracer-ics2022 <tracer@njuics.org>
2022-09-06 20:31:50 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:31:50 up 58 min,  1 user,  load average: 0.76, 0.46, 0.36

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for(int i = 0; i < 32; i++) 
-    printf("%s      %x      %d\n", regs[i], gpr(i), gpr(i));
+    printf("%s      0x%x      %d\n", regs[i], gpr(i), gpr(i));
   return ;
 }
 
