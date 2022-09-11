6fb51955964cdd512000932c55bb33ff1dfc19b8
tracer-ics2022 <tracer@njuics.org>
2022-09-11 21:05:49 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:49 up 43 min,  1 user,  load average: 0.25, 0.12, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -256,7 +256,7 @@ int find_op(int p, int q) {
 
   for(int i = 0; i < PRIORS; i++) 
     if(prior[i]) { 
-      Log("domiant OP is %s at pos %d", tokens[prior[i]].str, prior[i] - 1);
+      Log("domiant OP is %s at pos %d", tokens[prior[i] - 1].str, prior[i] - 1);
       return prior[i] - 1;
     }
   Log("OP can't be found"); assert(0);
