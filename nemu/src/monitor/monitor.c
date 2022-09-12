4381b185f1afe9ff884334cd376e2f01625b2034
tracer-ics2022 <tracer@njuics.org>
2022-09-12 20:15:39 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:15:39 up  3:33,  1 user,  load average: 0.55, 0.46, 0.38

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -26,6 +26,7 @@ void init_disasm(const char *triple);
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
+  Log("Trace: %s", MUXDEF(CONFIG_WATCHPOINT, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig"));
