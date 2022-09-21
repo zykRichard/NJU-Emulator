4f26e8cd1aed1350cfca79eae6a04dfe3773f7c7
tracer-ics2022 <tracer@njuics.org>
2022-09-21 12:13:26 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:13:26 up 14 min,  1 user,  load average: 0.36, 0.21, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -204,7 +204,7 @@ static bool check_parenthese(int p, int q) {
     else if(tokens[pr].type == ')') cnt --;
 
     if(cnt < 0) {
-      Log("Invalid parenthese match at %d", pr);
+      //Log("Invalid parenthese match at %d", pr);
       return false;
     }
 
@@ -256,7 +256,7 @@ int find_op(int p, int q) {
 
   for(int i = 0; i < PRIORS; i++) 
     if(prior[i]) { 
-      Log("domiant OP is %s at pos %d", tokens[prior[i] - 1].str, prior[i] - 1);
+      //Log("domiant OP is %s at pos %d", tokens[prior[i] - 1].str, prior[i] - 1);
       return prior[i] - 1;
     }
   Log("OP can't be found"); assert(0);
