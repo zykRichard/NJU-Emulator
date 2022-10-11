3d4a1db6c3305f2ba50a8692bbf2d312fbc24540
tracer-ics2022 <tracer@njuics.org>
2022-10-11 15:14:05 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:14:05 up  5:10,  1 user,  load average: 0.78, 0.55, 0.44

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -43,7 +43,7 @@ void sdb_set_batch_mode();
 
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
-static char *img_file = "~/ics2022/am-kernels/tests/cpu-tests/build/hello-str-riscv32-nemu.elf";
+static char *img_file = "~/ics2022/am-kernels/tests/cpu-tests/build/hello-str-riscv32-nemu.bin";
 
 static int difftest_port = 1234;
 
