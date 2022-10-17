de6a65aaf78564ce2481b12eabcad12889b3534a
tracer-ics2022 <tracer@njuics.org>
2022-10-17 20:36:10 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:36:10 up  9:46,  1 user,  load average: 0.49, 0.48, 0.46

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,14 +1,19 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 #include <stdint.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static int max_w ;
+static int max_h ;
 
 void __am_gpu_init() {
   int i;
   int w = inl(VGACTL_ADDR) >> 16;
   int h = inl(VGACTL_ADDR) & (0xffff);
+  max_w = w;
+  max_h = h;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for(i = 0; i < w * h; i++) fb[i] = i;
   outl(SYNC_ADDR, 1);  
@@ -27,12 +32,17 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int block_h = ctl -> h;
   //int block_sz = block_w * block_h;
   int fx = ctl -> x, fy = ctl -> y;
-  uint32_t **fb = (uint32_t **)(uintptr_t)FB_ADDR;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pixels = (uint32_t *)(ctl -> pixels);
 
-  for(int i = 0; i < block_w; i++)
-    for(int j = 0; j < block_h; j++)
-      fb[fx + i][fy + j] = pixels[i * j];
+  int start_id = max_w * fy + fx;
+  int x_bound = fx + block_w > max_w ? max_w : fx + block_w;
+  int y_bound = fy + block_h > max_h ? max_h : fy + block_h;
+    
+  for(int i = 0; i < y_bound - fy; i++) {
+    memcpy(fb + start_id + i * max_w, pixels, x_bound - fx);
+    pixels += block_w; 
+  }
 
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
