cad5d298e5754900ddfc7de9e56960fc74a021ff
tracer-ics2022 <tracer@njuics.org>
2022-11-14 20:32:18 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-48-generic #54~20.04.1-Ubuntu SMP Thu Sep 1 16:17:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:32:18 up  5:00,  1 user,  load average: 0.69, 0.38, 0.33

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,36 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ELFheader;
+  Elf_Phdr PROheader;
+
+  size_t base = 0;
+
+  // read ELF header:
+  ramdisk_read((void *)(&ELFheader), base, sizeof(Elf_Ehdr));
+  
+  // Judge:
+  assert((uint32_t)ELFheader.e_ident == 0x7f454c46);
+
+  uintptr_t ret_addr = ELFheader.e_entry;
+  size_t pro_off = ELFheader.e_phoff;
+  size_t pro_num = ELFheader.e_phnum;
+  size_t pro_size = ELFheader.e_phentsize;
+
+  // read Segment headers and LOAD:
+  for(int i = 0; i < pro_num; i++){
+
+    ramdisk_read((void *)(&PROheader), base + pro_off + i * pro_size, pro_size);
+    if(PROheader.p_type == PT_LOAD)
+      ramdisk_read((void *)(PROheader.p_vaddr), base + PROheader.p_offset, PROheader.p_memsz);
+    
+  }  
+
+  return ret_addr;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
