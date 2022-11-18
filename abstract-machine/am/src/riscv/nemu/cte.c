6394ec076ac47fd66f709c353663b47aaa0148dc
tracer-ics2022 <tracer@njuics.org>
2022-11-18 23:39:58 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:39:58 up  1:22,  1 user,  load average: 0.58, 0.54, 0.56

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
      printf("mcause is %x\n", c -> mcause);
     switch (c->mcause) {
-      case -1 : 
+      case 1 : 
         ev.event = EVENT_YIELD;
         break;
 
