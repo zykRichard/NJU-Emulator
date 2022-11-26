e01308b6364405ffbc8d73bc232542e711f58155
tracer-ics2022 <tracer@njuics.org>
2022-11-26 15:45:51 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:45:51 up  5:17,  1 user,  load average: 0.54, 0.43, 0.37

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,29 @@
 #include <common.h>
 
+static void do_syscall(Context* c) {
+
+  switch (c -> mcause)
+  {
+  case 0:         // exit
+    printf("exit syscall occurs\n"); 
+    break;
+  
+  case 1:        // yield
+    yield();
+    break;
+
+  default: printf("syscall#%d has not been implemented.\n", c -> mcause);
+    break;
+  }
+}
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 
     case EVENT_YIELD: printf("Yield occurs!\n"); break;
 
-    case EVENT_SYSCALL: printf("syscall occurs!\n"); break;    
+    case EVENT_SYSCALL: do_syscall(c); break;  
+     
     default: panic("Unhandled event ID = %d", e.event);
   }
 
