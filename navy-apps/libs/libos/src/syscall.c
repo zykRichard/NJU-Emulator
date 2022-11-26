932d416fc1becf1ff4dd9c5e584b299ee73f7583
tracer-ics2022 <tracer@njuics.org>
2022-11-26 16:18:36 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:18:36 up  5:50,  1 user,  load average: 0.46, 0.39, 0.37

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,8 +62,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   int ret = _syscall_(SYS_write, fd, buf, count);
-  _exit(SYS_write);
-  return 0;
+  return ret;
 }
 
 void *_sbrk(intptr_t increment) {
