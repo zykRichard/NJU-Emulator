20fbda0777f63cf5db1f98114bf3003259525023
tracer-ics2022 <tracer@njuics.org>
2022-11-26 19:56:58 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:56:58 up  9:28,  1 user,  load average: 0.67, 0.45, 0.38

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -47,25 +47,19 @@ void init_irq(void) {
 
 
 static void sys_exit(int code) { 
-  #ifdef CONFIG_STRACE 
   Log("sys_exit occurs");
-  #endif
 
   halt(code); 
 }
 
 static void sys_yield() { 
-  #ifdef CONFIG_STRACE
   Log("sys_yield occurs");
-  #endif
 
   yield(); 
 }
 
 static int sys_write(int fd, void *buf, size_t count) {
-  #ifdef CONFIG_STRACE
   Log("sys_write occurs");
-  #endif
 
   char *buf_wr = (char *)buf;
   if(fd == 1 || fd == 2)
