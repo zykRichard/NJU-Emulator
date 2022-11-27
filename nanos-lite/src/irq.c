2931af14065d96a29672892203a19717e3935478
tracer-ics2022 <tracer@njuics.org>
2022-11-27 17:25:36 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:25:36 up  2:36,  1 user,  load average: 0.68, 0.99, 0.95

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,34 +1,32 @@
 #include <common.h>
 
-static void sys_exit(int code); 
-static void sys_yield(); 
-static int sys_write(int fd, void *buf, size_t count);
-static int sys_brk();
 
-static void do_syscall(Context* c) {
 
-  switch (c -> GPR1)
-  {
-  case 0:         // exit
-    sys_exit(c -> GPR2);
-    break;
-  
-  case 1:        // yield
-    sys_yield();
-    break;
-  
-  case 4:        // write
-    c -> GPRx = sys_write((int)(c -> GPR2), (void*)(c -> GPR3), (size_t)(c -> GPR4));
-    break;
-  
-  case 9:       // brk
-    c -> GPRx = sys_brk();
-    break;
+// static void do_syscall(Context* c) {
 
-  default: printf("syscall#%d has not been implemented.\n", c -> mcause);
-    break;
-  }
-}
+//   switch (c -> GPR1)
+//   {
+//   case 0:         // exit
+//     sys_exit(c -> GPR2);
+//     break;
+  
+//   case 1:        // yield
+//     sys_yield();
+//     break;
+  
+//   case 4:        // write
+//     c -> GPRx = sys_write((int)(c -> GPR2), (void*)(c -> GPR3), (size_t)(c -> GPR4));
+//     break;
+  
+//   case 9:       // brk
+//     c -> GPRx = sys_brk();
+//     break;
+
+//   default: printf("syscall#%d has not been implemented.\n", c -> mcause);
+//     break;
+//   }
+// }
+void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
@@ -49,33 +47,3 @@ void init_irq(void) {
 }
 
 
-/****************************syscall********************************/
-
-static void sys_exit(int code) { 
-  Log("sys_exit occurs");
-
-  halt(code); 
-}
-
-static void sys_yield() { 
-  Log("sys_yield occurs");
-
-  yield(); 
-}
-
-static int sys_write(int fd, void *buf, size_t count) {
-  Log("sys_write occurs");
-
-  char *buf_wr = (char *)buf;
-  if(fd == 1 || fd == 2)
-  for(int i = 0; i < count; i++)
-    putch(buf_wr[i]);
-
-  return count;
-}
-
-static int sys_brk() {
-  Log("sys_brk occurs");
-
-  return 0;
-}
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,68 @@
 #include <common.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
 
-  switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+static void sys_exit(int code); 
+static void sys_yield(); 
+static int sys_write(int fd, void *buf, size_t count);
+static int sys_brk();
+
+void do_syscall(Context *c) {
+  uintptr_t arg[4];
+  arg[0] = c->GPR1;
+  arg[1] = c->GPR2;
+  arg[2] = c->GPR3;
+  arg[3] = c->GPR4;
+
+  switch (arg[0]) {
+      
+      case SYS_exit:
+        sys_exit(arg[1]);
+        break;
+
+      case SYS_yield:
+        sys_yield();
+        break;
+
+      case SYS_write:
+        c->GPRx = sys_write((int)arg[1], (void*)arg[2], (size_t)arg[3]);
+        break;
+
+      case SYS_brk:
+        c->GPRx = sys_brk();
+        break;
+
+      default: printf("syscall#%d has not been implemented.\n", c -> mcause);
+        break;
   }
+}
+
+/****************************syscall********************************/
+
+static void sys_exit(int code) { 
+  Log("sys_exit occurs");
+
+  halt(code); 
+}
+
+static void sys_yield() { 
+  Log("sys_yield occurs");
+
+  yield(); 
+}
+
+static int sys_write(int fd, void *buf, size_t count) {
+  Log("sys_write occurs");
+
+  char *buf_wr = (char *)buf;
+  if(fd == 1 || fd == 2)
+  for(int i = 0; i < count; i++)
+    putch(buf_wr[i]);
+
+  return count;
+}
+
+static int sys_brk() {
+  Log("sys_brk occurs");
+
+  return 0;
 }
