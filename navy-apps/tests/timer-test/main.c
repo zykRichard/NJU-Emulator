5bb0fb231186571147cbc0b057ffa8e64b1059c8
tracer-ics2022 <tracer@njuics.org>
2022-12-12 21:01:10 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:01:10 up 52 min,  1 user,  load average: 0.37, 0.31, 0.35

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <sys/time.h>
 int main(){
     int cnt = 0;
-    while(++cnt){
-        printf("Hello world from time %d\n", cnt);
-        sleep(1);
+    struct timeval tv1;
+    struct timeval tv2;
+    gettimeofday(&tv1, NULL);
+    while(1){
+        gettimeofday(&tv2, NULL);
+        if(tv2.tv_sec - tv1.tv_sec >= 1){
+            tv1 = tv2;
+            printf("Hello world from %d\n", cnt);
+            cnt ++;
+        }
     }
     return 0;
 }
