1708d965a0f343b96b730c67f040c096fb9dd827
tracer-ics2022 <tracer@njuics.org>
2022-12-21 18:46:30 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:46:30 up  7:41,  1 user,  load average: 0.82, 0.72, 0.65

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,7 +1,11 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
+#include <assert.h>
 
 #define keyname(k) #k,
+
+static uint8_t sdl_keystate[256] = {0};
 
 static const char *keyname[] = {
   "NONE",
@@ -13,11 +17,44 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  char buf[64] = {0};
+  if(NDL_PollEvent(buf, sizeof(buf)) == 0){
+    ev -> type = SDL_USEREVENT;
+    ev -> key.keysym.sym = 0;
+    return 0;
+  }
+  buf[strlen(buf) - 1] = 0;
+  if(buf[1] == 'u') ev -> type = SDL_KEYUP;
+  else if(buf[1] == 'd') ev -> type = SDL_KEYDOWN;
+  
+  for(int i = 1; i < sizeof(keyname) / sizeof(char*); i++)
+    if(strcmp(keyname[i], &buf[3]) == 0) {
+      ev -> key.keysym.sym = i;
+      switch(ev -> type) {
+        case SDL_KEYUP: sdl_keystate[i] = 0; break;
+        case SDL_KEYDOWN: sdl_keystate[i] = 1; break;
+      }
+      return 1;
+    }
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[64] = {0};
+  while(NDL_PollEvent(buf, sizeof(buf)) == 0);
+  buf[strlen(buf) - 1] = 0;
+  if(buf[1] == 'u') event -> type = SDL_KEYUP;
+  else if(buf[1] == 'd') event -> type = SDL_KEYDOWN;
+  
+  for(int i = 1; i < sizeof(keyname) / sizeof(char*); i++)
+    if(strcmp(keyname[i], &buf[3]) == 0) {
+      event -> key.keysym.sym = i;
+      switch(event -> type) {
+        case SDL_KEYUP: sdl_keystate[i] = 0; break;
+        case SDL_KEYDOWN: sdl_keystate[i] = 1; break;
+      }
+      return 1;
+    }
+  assert(0);
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
@@ -25,5 +62,18 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+  SDL_Event ev;
+  if(SDL_PollEvent(&ev)){
+    switch (ev.type)
+    {
+    case SDL_KEYUP:
+      sdl_keystate[ev.key.keysym.sym] = 0;
+      break;
+    
+    case SDL_KEYDOWN:
+      sdl_keystate[ev.key.keysym.sym] = 1;
+      break;
+    }
+  }
+  return sdl_keystate;
 }
