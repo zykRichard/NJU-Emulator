001bb49e56d9adf8e27f946d061cf62d669d76cf
tracer-ics2022 <tracer@njuics.org>
2022-12-22 23:46:38 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:46:38 up 12:46,  1 user,  load average: 0.65, 0.41, 0.97

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,7 +20,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   int fd = fs_open(filename, 0, 0); 
   size_t base = file_load(fd);
-  Log("base is %d", base);
+  //Log("base is %d", base);
   // read ELF header:
   ramdisk_read((void *)(&ELFheader), base, sizeof(Elf_Ehdr));
   //char *p = (char *)(&ELFheader);
@@ -30,20 +30,20 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   assert(*(uint32_t*)(ELFheader.e_ident) == 0x464c457f);
 
   uintptr_t ret_addr = ELFheader.e_entry;
-  printf("ret addr is %p\n", ret_addr);
+  //printf("ret addr is %p\n", ret_addr);
   size_t pro_off = ELFheader.e_phoff;
-  printf("Segment offset is %x\n", pro_off);
+  //printf("Segment offset is %x\n", pro_off);
   size_t pro_num = ELFheader.e_phnum;
-  printf("Segment number is %x\n", pro_num);
+  //printf("Segment number is %x\n", pro_num);
   size_t pro_size = ELFheader.e_phentsize;
-  printf("Segment entry size is %x\n", pro_size);
+  //printf("Segment entry size is %x\n", pro_size);
 
   // read Segment headers and LOAD:
   for(int i = 0; i < pro_num; i++){
 
     ramdisk_read((void *)(&PROheader), base + pro_off + i * pro_size, pro_size);
     if(PROheader.p_type == PT_LOAD){
-      printf("In proheader %d, offset is %d, size is %d\n", i, PROheader.p_offset, PROheader.p_memsz);
+      //printf("In proheader %d, offset is %d, size is %d\n", i, PROheader.p_offset, PROheader.p_memsz);
       ramdisk_read((void *)(PROheader.p_vaddr), base + PROheader.p_offset, PROheader.p_filesz);
       memset((void*)(PROheader.p_vaddr + PROheader.p_filesz), 0, PROheader.p_memsz - PROheader.p_filesz);
     }    
