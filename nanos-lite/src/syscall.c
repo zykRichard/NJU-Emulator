469a10594ee7b084295f90fb4bf85bb5cef01f4e
tracer-ics2022 <tracer@njuics.org>
2023-01-02 16:41:19 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:41:19 up 37 min,  1 user,  load average: 0.75, 0.38, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -79,7 +79,7 @@ static void sys_yield() {
 }
 
 static int sys_write(int fd, void *buf, size_t count) {
-  //Log("sys_write occurs");
+  Log("sys_write occurs");
   //Log("writing %d bytes", count);
   char *buf_wr = (char *)buf;
   if(fd == 1 || fd == 2)
@@ -92,19 +92,19 @@ static int sys_write(int fd, void *buf, size_t count) {
 }
 
 static int sys_brk() {
-  //Log("sys_brk occurs");
+  Log("sys_brk occurs");
 
   return 0;
 }
 
 static int sys_open(char* pathname, int flags, int modes) {
-  //Log("sys_open occurs");
+  Log("sys_open occurs");
   
   return fs_open(pathname, flags, modes);
 }
 
 static size_t sys_read(int fd, void *buf, size_t count) {
-  //Log("sys_read occurs");
+  Log("sys_read occurs");
   //Log("reading %p bytes", count); 
   size_t byte_read = fs_read(fd, buf, count);
   if(byte_read <= 0) {
@@ -115,7 +115,7 @@ static size_t sys_read(int fd, void *buf, size_t count) {
 }
 
 static off_t sys_lseek(int fd, off_t offset, int whence) {
-  //Log("sys_lseek occurs");
+  Log("sys_lseek occurs");
 
   off_t ret = fs_lseek(fd, offset, whence);
   
