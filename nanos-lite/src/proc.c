c6e3f7a3a6e77a33667401dd1fb7f3a654f7a053
tracer-ics2022 <tracer@njuics.org>
2023-01-06 00:59:02 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:59:02 up  1:37,  1 user,  load average: 0.94, 1.24, 1.22

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/menu");
+  naive_uload(NULL, "/share/music/rhythm/Do.ogg");
 }
 
 Context* schedule(Context *prev) {
