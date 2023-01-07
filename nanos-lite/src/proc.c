7213d3c53ddd9b44fa5a3eeabcda4dc5ab87a331
tracer-ics2022 <tracer@njuics.org>
2023-01-07 18:21:46 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:21:46 up  2:01,  1 user,  load average: 0.49, 0.31, 0.19

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,8 +22,8 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, (void *)('1'));
-  context_kload(&pcb[1], hello_fun, (void *)('2'));
+  context_kload(&pcb[0], hello_fun, (void *)'1');
+  context_kload(&pcb[1], hello_fun, (void *)'2');
   switch_boot_pcb();
 
   Log("Initializing processes...");
