88625ba75689ab9609b571a90f3345224b13756b
tracer-ics2022 <tracer@njuics.org>
2023-01-07 21:05:07 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:07 up  4:45,  1 user,  load average: 0.58, 0.41, 0.37

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -41,7 +41,7 @@ void init_proc() {
 Context* schedule(Context *prev) {
   printf("ready to schedule\n"); 
   current -> cp = prev;
-  current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
-
+  //current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
+  current = &pcb[1];
   return current -> cp;
 }
