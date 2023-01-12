09872a5327bcd301cc21ff2e97a2005b2c37f490
tracer-ics2022 <tracer@njuics.org>
2023-01-13 00:43:00 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:43:00 up 12:58,  1 user,  load average: 0.96, 0.66, 0.50

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -23,8 +23,8 @@
 int isa_mmu_check(vaddr_t vaddr, int len, int type) {
   int mode = cpu.satp.sig.MODE;
   switch(mode) {
-    case Bare : Log("mmu direct");return MMU_DIRECT;
-    case Sv32 : Log("mmu translate");return MMU_TRANSLATE;
+    case Bare : return MMU_DIRECT;
+    case Sv32 : return MMU_TRANSLATE;
     default : panic("mode error");return MMU_FAIL;
   }
   return -1;
