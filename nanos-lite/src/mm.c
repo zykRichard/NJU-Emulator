d4f274faada43c50c45161263b36ed9622392699
tracer-ics2022 <tracer@njuics.org>
2023-01-12 22:18:06 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:18:06 up 10:33,  1 user,  load average: 0.40, 0.32, 0.27

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -11,7 +11,11 @@ void* new_page(size_t nr_page) {
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-  return NULL;
+  size_t nr_page = n / PGSIZE;
+  void *ret = new_page(nr_page);
+  memset(ret, 0, n);
+  Log("alloc page at address %p", ret);
+  return ret;
 }
 #endif
 
