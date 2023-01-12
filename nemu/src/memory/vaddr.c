cc2b00a6895cc0fa725f44a4bee696e669b8f674
tracer-ics2022 <tracer@njuics.org>
2023-01-13 00:42:33 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:42:33 up 12:57,  1 user,  load average: 0.60, 0.58, 0.47

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -16,14 +16,35 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
+// int isa_mmu_check(vaddr_t vaddr, int len, int type);
+// paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type);
+
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  int mode = isa_mmu_check(addr, len, 0);
+  switch(mode) {
+    case MMU_DIRECT : return paddr_read(addr, len);
+    case MMU_TRANSLATE : return paddr_read(isa_mmu_translate(addr, len, 0), len);
+    default : panic("invalid mode");
+  }
+  return -1;
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  int mode = isa_mmu_check(addr, len, 0);
+  switch(mode) {
+    case MMU_DIRECT : return paddr_read(addr, len);
+    case MMU_TRANSLATE : return paddr_read(isa_mmu_translate(addr, len, 0), len);
+    default : panic("invalid mode");
+  }
+  return -1;
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  paddr_write(addr, len, data);
+  int mode = isa_mmu_check(addr, len, 0);
+  switch(mode) {
+    case MMU_DIRECT : paddr_write(addr, len, data); break;
+    case MMU_TRANSLATE : paddr_write(isa_mmu_translate(addr, len, 0), len, data); break;
+    default : panic("invalid mode");
+  }
+  
 }
