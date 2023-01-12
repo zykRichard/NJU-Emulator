5678c943223dc190a339d22680773bf8555384f4
tracer-ics2022 <tracer@njuics.org>
2023-01-12 12:47:26 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:47:26 up  1:02,  1 user,  load average: 0.77, 0.76, 0.50

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -31,7 +31,8 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 
-    case EVENT_YIELD: {printf("yield detected\n");c = schedule(c); break;}
+    case EVENT_YIELD: {//printf("yield detected\n");
+    c = schedule(c); break;}
 
     case EVENT_SYSCALL: do_syscall(c); break;  
      
