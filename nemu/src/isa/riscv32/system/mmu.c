4458613f743f0d138a499c7db68bdc9fba735185
tracer-ics2022 <tracer@njuics.org>
2023-01-13 12:02:01 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:02:01 up 38 min,  1 user,  load average: 0.31, 0.24, 0.10

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -47,7 +47,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t PTE_addr = (PDE >> 12) * PAGESIZE + vpn0 * PTESIZE;
   word_t PTE = paddr_read(PTE_addr, 4);
   if(!(PTE & 0x1))
-    panic("PTE invalid when reading addr %x, PTE is %x, PDE is %x", vaddr, PTE, PDE);
+    panic("PTE invalid when reading addr %x, PTE is %x, PDE is %x, vpn0: %x, vpn1 : %x, vpn_off : %x", vaddr, PTE, PDE, vpn0, vpn1, vpn_off);
   
   // paddr : PTE.PPN * PGSIZE + vpn_off
   paddr_t ret = (PTE >> 12) * PAGESIZE + vpn_off;
