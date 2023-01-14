8f222dae8c8cbec446a6907184354a94f8caa149
tracer-ics2022 <tracer@njuics.org>
2023-01-14 23:14:13 +0800

    >  compile NEMU 201850187 周琰轲 Linux richard 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:14:13 up 11:36,  1 user,  load average: 0.86, 0.87, 0.66

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,9 +29,9 @@ void init_proc() {
   char * envp2[] = {NULL};
   context_uload(&pcb[2], "/bin/bird", argv2, envp2);
 
-  char * argv3[] = {"/bin/nslider", NULL};
+  char * argv3[] = {"/bin/nterm", NULL};
   char * envp3[] = {NULL};
-  context_uload(&pcb[3], "/bin/bird", argv3, envp3);
+  context_uload(&pcb[3], "/bin/nterm", argv3, envp3);
   //char * argv0[] = {"/bin/pal", "--skip", NULL};
   //char * envp0[] = {NULL};
   //context_uload(&pcb[1], "/bin/nterm", argv1, envp1);
